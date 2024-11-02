e239c893f53639d9a81825e393fc6e18a88c1ccb
jpbarragan <jpbarragann@gmail.com>
2024-11-02 16:38:15 -0400

    Created FrontPage

--- src/App.js
+++ src/App.js
@@ -6,20 +6,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          My personal website.
-
-        </p>
+        {/*<img src={logo} className="App-logo" alt="logo" />*/}
+        
         <FrontPage/>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
--- src/components/FrontPage.js
+++ src/components/FrontPage.js
@@ -1,8 +1,15 @@
+
 function FrontPage () {
 
     return (
         <div>
-            <h1>THE FRONTPAGE OF MY WEBSITE</h1>    
+            <h1>Jose Pablo Barragan</h1>    
+            <br></br>
+            <h2>About</h2>
+            <h2>Books</h2>
+            <h2>Poems</h2>
+            <h2>Translations</h2>
+            <h2>Contact</h2>
         </div>
     );
 }
